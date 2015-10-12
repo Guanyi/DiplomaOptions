fcd703b3e8deb656c1290cd7872b62ecb45f005c
guanyi <guanyifang@hotmail.com>
2015-10-12 06:05:53 -0700

    almost only no same option in one choice left

--- OptionWebsite/Controllers/HomeController.cs
+++ OptionWebsite/Controllers/HomeController.cs
@@ -15,12 +15,15 @@ namespace OptionWebsite.Controllers
         [Authorize]
         public ActionResult Index()
         {
+            //from the option table, only select the active options, pass these records with OptionId to be drop down menu value
+            //Title will be the drop down menu text/
             ViewBag.OptionList = new SelectList(db.Options.Where(o => o.IsActive == true).OrderBy(o => o.Title), "OptionId", "Title");
+            
+            //if the login user is not admin, then get the student's student number, put it in the Student Id text box.
             if(!User.IsInRole("Admin"))
             {
                 ViewBag.CurrentStudentId = User.Identity.Name;
             }
-            
             return View();
         }
 
@@ -29,20 +32,25 @@ namespace OptionWebsite.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(Choice choice)
         {
-            //YearTerm selectedYearTerm = db.YearTerms.SingleOrDefault(y => y.IsDefault == true);
+            //get the only default YearTerm row by BCIT option policy
             choice.YearTerm = db.YearTerms.SingleOrDefault(y => y.IsDefault == true);
             
             //check the input model is valid and duplicate rows with the same StudentId in the StudentOptionChoices is not allowed in the same YearTerm
             if (ModelState.IsValid && !db.Choices.Any(c => c.StudentId == choice.StudentId && c.YearTerm.YearTermId == choice.YearTerm.YearTermId))
             {
                 choice.SelectionDate = DateTime.Now;
-                //choice.YearTerm.YearTermId = selectedYearTerm.YearTermId;
                 db.Choices.Add(choice);
                 db.SaveChanges();
+                TempData["success"] = true;
                 return RedirectToAction("Index");
             }
             ViewBag.OptionList = new SelectList(db.Options.Where(o => o.IsActive == true).OrderBy(o => o.Title), "OptionId", "Title");
-            return View(choice);
+            ModelState.AddModelError("", "You can only select option once in one year term.");
+            if (!User.IsInRole("Admin"))
+            {
+                ViewBag.CurrentStudentId = User.Identity.Name;
+            }
+            return View();
         }
     }
 }
